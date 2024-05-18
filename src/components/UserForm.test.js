9088e42ed8747475fffeb1951c921e84453a1fed
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2024-05-19 00:34:29 +0530

    add tests for user form

---
+++ src/components/UserForm.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import UserForm from "./UserForm";
+
+test("it shows two inputs and a button", () => {
+  render(<UserForm />);
+
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+test("it calls onUserAdd when the form is submitted", async () => {
+  // const argList = [];
+  // const callback = (...args) => {
+  //   console.log("args: ", args);
+  //   argList.push(args);
+  //   console.log("list: ", argList);
+  // };
+  const mock = jest.fn();
+  render(<UserForm onUserAdd={mock} />);
+
+  // Find the two inputs
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  // Simulate typing in a name
+  await user.click(nameInput);
+  await user.keyboard("jane");
+
+  // Simulate typing in an email
+  await user.click(emailInput);
+  await user.keyboard("jane@jane.com");
+
+  // Find the button
+  const button = screen.getByRole("button");
+  // Simulate clicking the button
+  await user.click(button);
+
+  // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "jane", email: "jane@jane.com" });
+});
