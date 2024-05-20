40c0e005492b2ae02c52e56963e24616d4b57782
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2024-05-20 12:49:43 +0530

    solution by data testid

---
+++ src/components/UserList.test.js
@@ -0,0 +1,26 @@
+import { render, screen, within } from "@testing-library/react";
+import UserList from "./UserList";
+
+const users = [
+  { name: "jane", email: "jane@jane.com" },
+  { name: "sam", email: "sam@sam.com" },
+];
+
+test("render correct table headers", () => {
+  render(<UserList users={users} />);
+  const nameHeader = screen.getByRole("columnheader", {
+    name: /name/i,
+  });
+  const emailHeader = screen.getByRole("columnheader", {
+    name: /email/i,
+  });
+  expect(nameHeader).toBeInTheDocument();
+  expect(emailHeader).toBeInTheDocument();
+});
+
+test("render one row per user", () => {
+  render(<UserList users={users} />);
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  expect(rows).toHaveLength(2);
+  screen.logTestingPlaygroundURL();
+});
